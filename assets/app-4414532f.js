3f08a336653c2d7ece5db909d945350a208c1470
Theodorus <67539175+dragonarmy26@users.noreply.github.com>
2025-02-26 15:56:30 +0700

    Update app-4414532f.js

--- assets/app-4414532f.js
+++ assets/app-4414532f.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Create elements
     const title = document.createElement('title');
-    title.textContent = 'Hello World';
+    title.textContent = 'TEST HOST HEADER INJECTION';
 
     const h1 = document.createElement('h1');
-    h1.textContent = 'Hello, World!';
+    h1.textContent = 'Hello visitor!';
 
     const p = document.createElement('p');
-    p.textContent = 'You just got attacked :)';
+    p.textContent = 'You just got redirected :)\nHope you did not input anything sensitive ;)';
+
+    const img = document.createElement('img');
+    img.src = 'https://media.tenor.com/EFDwfjT2GuQAAAAM/spinning-cat.gif';
+    img.alt = 'Spinning Cat';
 
     // Append elements to the document
     document.head.appendChild(title);
     document.body.appendChild(h1);
     document.body.appendChild(p);
+    document.body.appendChild(img);
 });
